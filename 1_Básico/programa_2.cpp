e49f4b5079c81b688da5f1989cde073430d5f389
Roberto Nóbrega <r_t_4@hotmail.com>
2023-03-05 17:43:41 -0300

    Commit 3

---
+++ 1_Básico/programa_2.cpp
@@ -0,0 +1,17 @@
+#include <iostream> // Importanto biblioteca de entrada e saída de dados.
+
+using namespace std; // Usando o std padrão, para "evitar conflito(s) de namespace".
+
+/*
+                Aula 5: Declarando Variáveis
+        C++ é "Case Sensitive", ou seja, distingue letras maiúsculas das minúsculas.
+            OBS: int idade, Idade; // São duas variáveis, com nomes diferentes.
+        Para declarar variáveis, tem que iniciar com letra ou com o underline.
+*/
+int main(void){
+    int idade; // Declarando uma variável, de tipo inteiro.
+    // int idade2, idade3; // Declarando duas variáveis, numa mesma linha de código.
+    idade = 20;
+    cout << "\n\tIdade Fornecida: " << idade << endl;
+    return 0;
+}
